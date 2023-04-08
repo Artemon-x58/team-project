0d3f9f04c51d8dad5d51fdd2222c399abfbf3b2c
Yevhenii <jekakrasov1996@gmail.com>
2023-04-08 16:20:59 +0200

    our-prod
    закончил секцию,слайдер работает не идеально

--- src/js/slider-our-products.js
+++ src/js/slider-our-products.js
@@ -1,6 +1,24 @@
 $('.our-products-slider').slick({
-    dots: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });  
+  dots: true,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 1158,
+      settings: {
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+      },
+    },
+  ],
+});
