a230036454c78f2cc3707b0d9b5617b5675d40c8
Nyekrasov <pnekrasov1910@gmail.com>
2023-04-09 18:27:03 +0300

    work moment

--- src/js/slider-reviews.js
+++ src/js/slider-reviews.js
@@ -2,5 +2,23 @@ $('.reviews-slider').slick({
     dots: true,
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
-  });  
+  slidesToScroll: 1,
+      responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        dots: true,
+        infinite: true,
+        slidesToShow: 2,
+      },
+    },
+  ],
+});
